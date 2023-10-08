3fd14195c8a0261652cecac6c28f402318c0bc8c
Sobowale Emmanuel <sobowalegz2@gmail.com>
2023-10-08 22:25:29 +0100

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,6 +7,6 @@ sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
+sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '24i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
